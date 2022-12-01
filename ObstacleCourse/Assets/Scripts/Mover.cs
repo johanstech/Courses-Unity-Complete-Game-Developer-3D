749f5369931e2a3a718de3348af4e9548e2ff45f
Johan Andersson <johanstechs@gmail.com>
2022-12-01 18:13:23 +0100

    More methods

--- ObstacleCourse/Assets/Scripts/Mover.cs
+++ ObstacleCourse/Assets/Scripts/Mover.cs
@@ -9,10 +9,22 @@ public class Mover : MonoBehaviour
 
   void Start()
   {
-
+    PrintInstructions();
   }
 
   void Update()
+  {
+    Move();
+  }
+
+  void PrintInstructions()
+  {
+    Debug.Log("Welcome to the game!");
+    Debug.Log("Move your player with WAS or the arrow keys.");
+    Debug.Log("Don't hit the walls!");
+  }
+
+  void Move()
   {
     float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
     float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
