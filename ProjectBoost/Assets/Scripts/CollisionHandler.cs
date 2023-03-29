2fa5c8cb2dabfe08275f659b9d57e09ddc28c87c
Johan <johanstechs@gmail.com>
2023-03-29 22:16:57 +0200

    Formatting

--- ProjectBoost/Assets/Scripts/CollisionHandler.cs
+++ ProjectBoost/Assets/Scripts/CollisionHandler.cs
@@ -2,19 +2,19 @@ using UnityEngine;
 
 public class CollisionHandler : MonoBehaviour
 {
-  void OnCollisionEnter(Collision other)
-  {
-    switch (other.gameObject.tag)
+    void OnCollisionEnter(Collision other)
     {
-      case "Friendly":
-        Debug.Log("This thing is friendly.");
-        break;
-      case "Finish":
-        Debug.Log("You reached the finish.");
-        break;
-      default:
-        Debug.Log("Sorry, you blew up.");
-        break;
+        switch (other.gameObject.tag)
+        {
+            case "Friendly":
+                Debug.Log("This thing is friendly.");
+                break;
+            case "Finish":
+                Debug.Log("You reached the finish.");
+                break;
+            default:
+                Debug.Log("Sorry, you blew up.");
+                break;
+        }
     }
-  }
 }
--- ProjectBoost/Assets/Scripts/Movement.cs
+++ ProjectBoost/Assets/Scripts/Movement.cs
@@ -2,58 +2,58 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-  [SerializeField]
-  float thrustPower = 1000f;
-  [SerializeField]
-  float rotationPower = 100f;
+    [SerializeField]
+    float thrustPower = 1000f;
+    [SerializeField]
+    float rotationPower = 100f;
 
-  Rigidbody _rb;
-  AudioSource _audioSource;
+    Rigidbody _rb;
+    AudioSource _audioSource;
 
-  void Start()
-  {
-    _rb = GetComponent<Rigidbody>();
-    _audioSource = GetComponent<AudioSource>();
-  }
-
-  void Update()
-  {
-    ProcessThrust();
-    ProcessRotation();
-  }
-
-  void ProcessThrust()
-  {
-    if (Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.UpArrow))
+    void Start()
     {
-      _rb.AddRelativeForce(Vector3.up * thrustPower * Time.deltaTime);
-      if (!_audioSource.isPlaying)
-      {
-        _audioSource.Play();
-      }
+        _rb = GetComponent<Rigidbody>();
+        _audioSource = GetComponent<AudioSource>();
     }
-    else
-    {
-      _audioSource.Stop();
-    }
-  }
 
-  void ProcessRotation()
-  {
-    if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+    void Update()
     {
-      ApplyRotation(rotationPower);
+        ProcessThrust();
+        ProcessRotation();
     }
-    else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
-    {
-      ApplyRotation(-rotationPower);
-    }
-  }
 
-  void ApplyRotation(float rotationThisFrame)
-  {
-    _rb.freezeRotation = true;
-    transform.Rotate(Vector3.forward * rotationThisFrame * Time.deltaTime);
-    _rb.freezeRotation = false;
-  }
+    void ProcessThrust()
+    {
+        if (Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.UpArrow))
+        {
+            _rb.AddRelativeForce(Vector3.up * thrustPower * Time.deltaTime);
+            if (!_audioSource.isPlaying)
+            {
+                _audioSource.Play();
+            }
+        }
+        else
+        {
+            _audioSource.Stop();
+        }
+    }
+
+    void ProcessRotation()
+    {
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            ApplyRotation(rotationPower);
+        }
+        else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            ApplyRotation(-rotationPower);
+        }
+    }
+
+    void ApplyRotation(float rotationThisFrame)
+    {
+        _rb.freezeRotation = true;
+        transform.Rotate(Vector3.forward * rotationThisFrame * Time.deltaTime);
+        _rb.freezeRotation = false;
+    }
 }
